1153bd7948e71d81a676c59e0e750544aac2fc37
Alvaro <Alvaro@LAPTOP-U3UUT94G>
2020-03-17 20:49:41 +0100

    Clase DatosPaquete

---
+++ gestionTFG/gestionTFG/DatosPaquete.vb
@@ -0,0 +1,73 @@
+﻿Public Class DatosPaquete
+    Dim _codigo As Integer
+    Dim _ancho As Integer
+    Dim _alto As Integer
+    Dim _peso As Double
+    Dim _origen As String
+    Dim _destino As String
+
+    Public Sub New()
+
+    End Sub
+    Public Sub New(ByVal cod As Integer, ByVal anc As Integer, ByVal alt As Integer, ByVal pes As Double, ByVal ori As String, ByVal des As String)
+        Me.codigo = cod
+        Me.ancho = anc
+        Me.alto = alt
+        Me.peso = pes
+        Me.origen = ori
+        Me.destino = des
+    End Sub
+    Public Property codigo As Integer
+        Get
+            Return Me._codigo
+        End Get
+        Set(value As Integer)
+            Me._codigo = value
+        End Set
+    End Property
+
+    Public Property ancho As Integer
+        Get
+            Return Me._ancho
+        End Get
+        Set(value As Integer)
+            Me._ancho = value
+        End Set
+    End Property
+
+    Public Property alto As Integer
+        Get
+            Return Me._alto
+        End Get
+        Set(value As Integer)
+            Me._alto = value
+        End Set
+    End Property
+
+    Public Property peso As Double
+        Get
+            Return Me._peso
+        End Get
+        Set(value As Double)
+            Me._peso = value
+        End Set
+    End Property
+
+    Public Property origen As String
+        Get
+            Return Me._origen
+        End Get
+        Set(value As String)
+            Me._origen = value
+        End Set
+    End Property
+
+    Public Property destino As String
+        Get
+            Return Me._destino
+        End Get
+        Set(value As String)
+            Me._destino = value
+        End Set
+    End Property
+End Class
--- gestionTFG/gestionTFG/etiquetaPaquete.Designer.vb
+++ gestionTFG/gestionTFG/etiquetaPaquete.Designer.vb
@@ -23,13 +23,24 @@ Partial Class etiquetaPaquete
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
         Me.SuspendLayout()
+        '
+        'ReportViewer1
+        '
+        Me.ReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ReportViewer1.Location = New System.Drawing.Point(0, 0)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.ServerReport.BearerToken = Nothing
+        Me.ReportViewer1.Size = New System.Drawing.Size(800, 450)
+        Me.ReportViewer1.TabIndex = 0
         '
         'etiquetaPaquete
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ReportViewer1)
         Me.Name = "etiquetaPaquete"
         Me.Text = "etiquetaPaquete"
         Me.ResumeLayout(False)
@@ -37,4 +48,5 @@ Partial Class etiquetaPaquete
     End Sub
 
     Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
 End Class
